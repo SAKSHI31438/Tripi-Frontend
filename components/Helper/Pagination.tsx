6bc1d1e166f06aa84fc63275430de674baed3422
SAKSHI31438 <kadamsakshi976@gmail.com>
2025-11-01 15:28:27 +0530

    first commit

--- components/Helper/Pagination.tsx
+++ components/Helper/Pagination.tsx
@@ -15,24 +15,28 @@ const Pagination = () => {
       <UIPagination>
         <PaginationContent>
           <PaginationItem>
-            <PaginationPrevious href="#" />
+            <PaginationPrevious href="#" size="default" />
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#">1</PaginationLink>
+            <PaginationLink href="#" size="default">
+              1
+            </PaginationLink>
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#" isActive>
+            <PaginationLink href="#" isActive size="default">
               2
             </PaginationLink>
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
+            <PaginationLink href="#" size="default">
+              3
+            </PaginationLink>
           </PaginationItem>
           <PaginationItem>
             <PaginationEllipsis />
           </PaginationItem>
           <PaginationItem>
-            <PaginationNext href="#" />
+            <PaginationNext href="#" size="default" />
           </PaginationItem>
         </PaginationContent>
       </UIPagination>
